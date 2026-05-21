502016e3bfc181a30a039cd22710712375628136
Ahmet <fizziomedia@gmail.com>
2026-05-21 17:39:38 +0300

    add ServiceMap below Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import ServiceMap from "./components/ServiceMap";
 import ProblemSolution from "./components/ProblemSolution";
 import Services from "./components/Services";
 import Process from "./components/Process";
@@ -14,6 +15,7 @@ export default function Home() {
     <main>
       <Navbar />
       <Hero />
+      <ServiceMap />
       <ProblemSolution />
       <Services />
       <Process />
